78bc89b13e927c1b78736aa56a12d6e875d20b5f
philipycha <philip.yc.ha@gmail.com>
2016-02-25 14:32:18 -0800

    Added files via upload
    Assignment + Checkpoint

---
+++ Blocstagram/Blocstagram/AppDelegate.m
@@ -0,0 +1,49 @@
+//
+//  AppDelegate.m
+//  Blocstagram
+//
+//  Created by Philip Ha on 2016-02-21.
+//  Copyright (c) 2016 Bloc. All rights reserved.
+//
+
+#import "AppDelegate.h"
+#import "ImagesTableViewController.h"
+#import "LoginViewController.h"
+#import "DataSource.h"
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    
+    [DataSource sharedInstance]; // create the data source (so it can receive the access token notification)
+    
+    UINavigationController *navVC = [[UINavigationController alloc] init];
+    
+    if (![DataSource sharedInstance].accessToken) {
+        
+    LoginViewController *loginVC = [[LoginViewController alloc] init];
+    [navVC setViewControllers:@[loginVC] animated:YES];
+    
+    [[NSNotificationCenter defaultCenter] addObserverForName:LoginViewControllerDidGetAccessTokenNotification object:nil queue:nil usingBlock:^(NSNotification *note) {
+        ImagesTableViewController *imagesVC = [[ImagesTableViewController alloc] init];
+        [navVC setViewControllers:@[imagesVC] animated:YES];
+    }];
+        
+    } else {
+        ImagesTableViewController *imagesVC = [[ImagesTableViewController alloc] init];
+        [navVC setViewControllers:@[imagesVC] animated:YES];
+    }
+    
+    self.window.rootViewController = navVC;
+    
+    self.window.backgroundColor = [UIColor whiteColor];
+    [self.window makeKeyAndVisible];
+    return YES;
+    
+}
+
+
+@end
+
